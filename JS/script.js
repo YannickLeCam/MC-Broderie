b88cc34d7220c0ca3c85a344770b2a420967e4c9
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-07-10 11:39:20 +0200

    add(card CSS , presentation carousel) del(carousel)

--- JS/script.js
+++ JS/script.js
@@ -52,5 +52,48 @@ startCarousel();
 
 // Arrête et reprend le carrousel au survol
 const carousel = document.getElementById('carousel');
+/*
 carousel.addEventListener('mouseenter', stopCarousel);
 carousel.addEventListener('mouseleave', startCarousel);
+*/
+
+let currentIndex2 = 0;
+
+function showSlide2(index) {
+    const slides2 = document.querySelectorAll('.carousel-item2');
+    const totalSlides2 = slides2.length;
+    
+    // Assure que l'index est dans les limites
+    if (index >= totalSlides2) {
+        currentIndex2 = 0;
+    } else if (index < 0) {
+        currentIndex2 = totalSlides2 - 1;
+    } else {
+        currentIndex2 = index;
+    }
+    
+    // Déplace les slides
+    const carouselInner2 = document.querySelector('.carousel-inner2');
+    carouselInner2.style.transform = 'translateX(' + (-currentIndex2 * 100) + '%)';
+}
+function nextSlide2() {
+    showSlide2(currentIndex2 + 1);
+}
+
+function startCarousel2() {
+    intervalId2 = setInterval(nextSlide2, 3000);
+}
+
+function stopCarousel2() {
+    clearInterval(intervalId2);
+}
+
+// Démarre le carrousel automatiquement
+startCarousel2();
+
+// Arrête et reprend le carrousel au survol
+const carousel2 = document.getElementById('carousel2');
+/*
+carousel2.addEventListener('mouseenter', stopCarousel2);
+carousel2.addEventListener('mouseleave', startCarousel2);
+*/
